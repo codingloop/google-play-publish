d136f587f5b3cc36609ae7e1c59af139d7cd57a8
codingloop <ishunfs@gmail.com>
2022-07-08 22:14:40 +0530

    Parse credentials method

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 import sys
 
+from src.read_config import read_config
+from src.utils import stop_exec
+
 
 def main(config_file, playstore_secret):
-    print(config_file)
-    print(playstore_secret)
+    try:
+        config = read_config(config_file)
+
+    except Exception as e:
+        print(e.args)
+        stop_exec()
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+from google.oauth2 import service_account
 
 
 def stop_exec(message: str = None):
@@ -6,3 +9,15 @@ def stop_exec(message: str = None):
         print(message)
 
     sys.exit(5)
+
+
+def get_google_credentials(encrypted_file: str, decryption_pwd: str):
+    SCOPES = ['https://www.googleapis.com/auth/androidpublisher']
+    os.system(f"gpg -d --passphrase {decryption_pwd} --batch {encrypted_file} > tmp/keystore.jks")
+    credentials = service_account.Credentials.from_service_account_file("tmp/keystore.jks", scopes=SCOPES)
+    os.removedirs("tmp")
+    return credentials
+
+
+
+
