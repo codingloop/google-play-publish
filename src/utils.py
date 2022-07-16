a22fa17730e4320bf250ed379fad1bc4637f61b8
codingloop <ishunfs@gmail.com>
2022-07-16 22:13:01 +0530

    Debug actions

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ def get_google_credentials(encrypted_file: str, decryption_pwd: str):
     os.makedirs("tmp", exist_ok=True)
     with open("tmp/secret_file.asc", "w") as f:
         f.write(encrypted_file)
-    os.system(f"gpg -d --passphrase {decryption_pwd} --batch tmp/secret_file.asc > tmp/client_secrets.json")
+    os.system(f"/usr/bin/gpg -d --passphrase {decryption_pwd} --batch tmp/secret_file.asc > tmp/client_secrets.json")
     credentials = service_account.Credentials.from_service_account_file("tmp/client_secrets.json", scopes=SCOPES)
     os.removedirs("tmp")
     return credentials
